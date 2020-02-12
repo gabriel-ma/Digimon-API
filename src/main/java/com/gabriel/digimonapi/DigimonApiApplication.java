3f97d9c8729816e7bbaf53bad91985f850ef7dd8
Gabriel Machado <gabrielmachado@5129-NB.local>
2020-02-11 22:44:12 -0300

    The very beginning of the Digimon Api

---
+++ src/main/java/com/gabriel/digimonapi/DigimonApiApplication.java
@@ -0,0 +1,12 @@
+package com.gabriel.digimonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigimonApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DigimonApiApplication.class, args);
+  }
+}
